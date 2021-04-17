6f7cce5ee7a4fe5e60563685368b398523e933b2
aviple-businessinnovator <76940206+aviple-businessinnovator@users.noreply.github.com>
2021-04-17 23:35:35 +0530

    firstcommit

---
+++ backend/build/precache-manifest.16bb5799dcc2d7dc2db0ab780dd05a0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21dfb10cea4e1cd32e8f",
+    "url": "/static/css/main.c171ea36.chunk.css"
+  },
+  {
+    "revision": "21dfb10cea4e1cd32e8f",
+    "url": "/static/js/main.21dfb10c.chunk.js"
+  },
+  {
+    "revision": "6803aa1d807a16e6ba20",
+    "url": "/static/js/1.6803aa1d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "109d0de1d264a589dd961420a1abaff6",
+    "url": "/index.html"
+  }
+];
